b79c48f031f78d6543fe6e7d727460095d0e7e7a
reoring <reoring@gmail.com>
2024-10-14 09:03:42 +0900

    chore: add machine template

--- cmd/main.go
+++ cmd/main.go
@@ -151,6 +151,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CAPTCluster")
 		os.Exit(1)
 	}
+	if err = (&controller.CAPTMachineTemplateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CAPTMachineTemplate")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {

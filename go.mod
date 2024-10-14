6fb6a9be279d94e3f1fb64895705334d0ad6bb75
reoring <reoring@gmail.com>
2024-10-14 09:31:30 +0900

    chore: impl captcluster controller

--- go.mod
+++ go.mod
@@ -5,18 +5,21 @@ go 1.22.0
 require (
 	github.com/onsi/ginkgo/v2 v2.19.0
 	github.com/onsi/gomega v1.33.1
+	github.com/upbound/provider-terraform v0.18.0
 	k8s.io/apimachinery v0.31.0
 	k8s.io/client-go v0.31.0
 	sigs.k8s.io/controller-runtime v0.19.0
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/crossplane/crossplane-runtime v1.16.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
@@ -40,7 +43,7 @@ require (
 	github.com/google/pprof v0.0.0-20240525223248-4bfdf5a9a2af // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
-	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -53,6 +56,7 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stoewer/go-strcase v1.2.0 // indirect
@@ -67,14 +71,14 @@ require (
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+	golang.org/x/exp v0.0.0-20240112132812-db7319d0e0e3 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/term v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240528184218-531527333157 // indirect
--- internal/controller/captcluster_controller.go
+++ internal/controller/captcluster_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -25,6 +26,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	infrastructurev1beta1 "github.com/appthrust/capt/api/v1beta1"
+	tfv1beta1 "github.com/upbound/provider-terraform/apis/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // CAPTClusterReconciler reconciles a CAPTCluster object
@@ -36,22 +40,134 @@ type CAPTClusterReconciler struct {
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=captclusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=captclusters/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=captclusters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=tf.upbound.io,resources=workspaces,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the CAPTCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *CAPTClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the CAPTCluster instance
+	captCluster := &infrastructurev1beta1.CAPTCluster{}
+	if err := r.Get(ctx, req.NamespacedName, captCluster); err != nil {
+		log.Error(err, "Unable to fetch CAPTCluster")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Check if the Terraform Workspace already exists
+	workspace := &tfv1beta1.Workspace{}
+	workspaceName := types.NamespacedName{
+		Name:      fmt.Sprintf("%s-workspace", captCluster.Name),
+		Namespace: captCluster.Namespace,
+	}
+
+	if err := r.Get(ctx, workspaceName, workspace); err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			log.Error(err, "Unable to fetch Terraform Workspace")
+			return ctrl.Result{}, err
+		}
+
+		// Workspace doesn't exist, create a new one
+		workspace = &tfv1beta1.Workspace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      workspaceName.Name,
+				Namespace: workspaceName.Namespace,
+			},
+			Spec: tfv1beta1.WorkspaceSpec{
+				ForProvider: tfv1beta1.WorkspaceParameters{
+					Module:       r.generateTerraformCode(captCluster),
+					Source:       tfv1beta1.ModuleSourceInline,
+					InlineFormat: tfv1beta1.FileFormatHCL,
+				},
+			},
+		}
+
+		if err := r.Create(ctx, workspace); err != nil {
+			log.Error(err, "Unable to create Terraform Workspace")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Created Terraform Workspace", "workspace", workspaceName)
+	} else {
+		// Workspace exists, update it
+		workspace.Spec.ForProvider.Module = r.generateTerraformCode(captCluster)
+		if err := r.Update(ctx, workspace); err != nil {
+			log.Error(err, "Unable to update Terraform Workspace")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Updated Terraform Workspace", "workspace", workspaceName)
+	}
+
+	// Update CAPTCluster status
+	captCluster.Status.WorkspaceName = workspace.Name
+	if err := r.Status().Update(ctx, captCluster); err != nil {
+		log.Error(err, "Unable to update CAPTCluster status")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *CAPTClusterReconciler) generateTerraformCode(cluster *infrastructurev1beta1.CAPTCluster) string {
+	// TODO: Implement the logic to generate Terraform code based on the CAPTCluster spec
+	return fmt.Sprintf(`
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "%s"
+  cluster_version = "%s"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = %t
+
+  # Add other EKS configurations based on CAPTCluster spec
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "%s-vpc"
+  cidr = "%s"
+
+  azs             = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
+  private_subnets = [for k, v in data.aws_availability_zones.available.names : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in data.aws_availability_zones.available.names : cidrsubnet(var.vpc_cidr, 8, k+48)]
+
+  enable_nat_gateway   = %t
+  single_nat_gateway   = %t
+  enable_dns_hostnames = true
+
+  public_subnet_tags = %s
+  private_subnet_tags = %s
+}
+
+data "aws_availability_zones" "available" {}
+
+variable "vpc_cidr" {
+  default = "%s"
+}
+`, cluster.Name, cluster.Spec.Version, cluster.Spec.PublicAccess, cluster.Name, cluster.Spec.VpcCIDR,
+		cluster.Spec.PublicAccess, true, // Assuming single NAT gateway for now
+		formatTags(cluster.Spec.PublicSubnetTags),
+		formatTags(cluster.Spec.PrivateSubnetTags),
+		cluster.Spec.VpcCIDR)
+}
+
+func formatTags(tags map[string]string) string {
+	if len(tags) == 0 {
+		return "{}"
+	}
+
+	result := "{\n"
+	for k, v := range tags {
+		result += fmt.Sprintf("    %s = \"%s\"\n", k, v)
+	}
+	result += "  }"
+
+	return result
 }
 
 // SetupWithManager sets up the controller with the Manager.

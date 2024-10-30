c7b26597a308a0b308d8ab574816dc1d8f0faae2
reoring <reoring@gmail.com>
2024-10-30 17:54:11 +0900

    chore: improve implementation

--- api/v1beta1/workspacetemplate_types.go
+++ api/v1beta1/workspacetemplate_types.go
@@ -18,94 +18,47 @@ package v1beta1
 
 import (
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	tfv1beta1 "github.com/upbound/provider-terraform/apis/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ModuleSourceType defines the type of module source
-type ModuleSourceType string
-
-const (
-	// ModuleSourceInline indicates the module is defined inline
-	ModuleSourceInline ModuleSourceType = "Inline"
-	// ModuleSourceGit indicates the module is sourced from a git repository
-	ModuleSourceGit ModuleSourceType = "Git"
-)
-
-// Variable defines a variable to be passed to the Terraform module
-type Variable struct {
-	// Key is the name of the variable
-	Key string `json:"key"`
-
-	// Value is the value of the variable
-	// +optional
-	Value string `json:"value,omitempty"`
-
-	// ValueFrom allows the value to be sourced from another resource
-	// +optional
-	ValueFrom *VariableSource `json:"valueFrom,omitempty"`
-}
-
-// VariableSource defines the source of a variable value
-type VariableSource struct {
-	// SecretKeyRef selects a key of a Secret
-	// +optional
-	SecretKeyRef *SecretKeySelector `json:"secretKeyRef,omitempty"`
-
-	// ConfigMapKeyRef selects a key of a ConfigMap
-	// +optional
-	ConfigMapKeyRef *ConfigMapKeySelector `json:"configMapKeyRef,omitempty"`
-}
-
-// SecretKeySelector selects a key of a Secret
-type SecretKeySelector struct {
-	// Name of the secret
-	Name string `json:"name"`
-
-	// Namespace of the secret
-	// +optional
-	Namespace string `json:"namespace,omitempty"`
-
-	// Key is the key in the secret
-	Key string `json:"key"`
-}
-
-// ConfigMapKeySelector selects a key of a ConfigMap
-type ConfigMapKeySelector struct {
-	// Name of the configmap
-	Name string `json:"name"`
-
-	// Namespace of the configmap
-	// +optional
-	Namespace string `json:"namespace,omitempty"`
-
-	// Key is the key in the configmap
-	Key string `json:"key"`
-}
-
 // WorkspaceTemplateSpec defines the desired state of WorkspaceTemplate
 type WorkspaceTemplateSpec struct {
-	// Module is the HCL format Terraform module
+	// Template defines the workspace template
 	// +kubebuilder:validation:Required
-	Module string `json:"module"`
-
-	// Source specifies how the module is sourced
-	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:Enum=Inline;Git
-	Source ModuleSourceType `json:"source"`
-
-	// Variables is a list of variables to pass to the Terraform module
-	// +optional
-	Variables []Variable `json:"variables,omitempty"`
-
-	// ProviderConfigRef specifies which provider config to use for the workspace
-	// +kubebuilder:validation:Required
-	ProviderConfigRef string `json:"providerConfigRef"`
+	Template WorkspaceTemplateDefinition `json:"template"`
 
 	// WriteConnectionSecretToRef specifies the namespace and name of a
 	// Secret to which any connection details for this managed resource should
 	// be written.
 	// +optional
 	WriteConnectionSecretToRef *xpv1.SecretReference `json:"writeConnectionSecretToRef,omitempty"`
+}
+
+// WorkspaceTemplateDefinition defines the template for creating workspaces
+type WorkspaceTemplateDefinition struct {
+	// Metadata contains template-specific metadata
+	// +optional
+	Metadata *WorkspaceTemplateMetadata `json:"metadata,omitempty"`
+
+	// Spec defines the desired state of the workspace
+	// +kubebuilder:validation:Required
+	Spec tfv1beta1.WorkspaceParameters `json:"spec"`
+}
+
+// WorkspaceTemplateMetadata contains metadata specific to the workspace template
+type WorkspaceTemplateMetadata struct {
+	// Description provides a human-readable description of the template
+	// +optional
+	Description string `json:"description,omitempty"`
+
+	// Version specifies the version of this template
+	// +optional
+	Version string `json:"version,omitempty"`
+
+	// Tags are key-value pairs that can be used to organize and categorize templates
+	// +optional
+	Tags map[string]string `json:"tags,omitempty"`
 }
 
 // WorkspaceTemplateStatus defines the observed state of WorkspaceTemplate
@@ -119,10 +72,10 @@ type WorkspaceTemplateStatus struct {
 	Conditions []xpv1.Condition `json:"conditions,omitempty"`
 }
 
-// +kubebuilder:object:root=true
-// +kubebuilder:subresource:status
-// +kubebuilder:printcolumn:name="WORKSPACE",type="string",JSONPath=".status.workspaceName"
-// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="WORKSPACE",type="string",JSONPath=".status.workspaceName"
+//+kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 
 // WorkspaceTemplate is the Schema for the workspacetemplates API
 type WorkspaceTemplate struct {
@@ -133,7 +86,7 @@ type WorkspaceTemplate struct {
 	Status WorkspaceTemplateStatus `json:"status,omitempty"`
 }
 
-// +kubebuilder:object:root=true
+//+kubebuilder:object:root=true
 
 // WorkspaceTemplateList contains a list of WorkspaceTemplate
 type WorkspaceTemplateList struct {
---
+++ internal/controller/workspacetemplate_controller_v2.go
@@ -0,0 +1,193 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	infrastructurev1beta1 "github.com/appthrust/capt/api/v1beta1"
+	tfv1beta1 "github.com/upbound/provider-terraform/apis/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	workspaceTemplateFinalizerV2 = "workspacetemplate.v2.infrastructure.cluster.x-k8s.io"
+)
+
+// WorkspaceTemplateReconcilerV2 reconciles a WorkspaceTemplate object
+type WorkspaceTemplateReconcilerV2 struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=workspacetemplates,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=workspacetemplates/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=workspacetemplates/finalizers,verbs=update
+//+kubebuilder:rbac:groups=tf.crossplane.io,resources=workspaces,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+func (r *WorkspaceTemplateReconcilerV2) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// Fetch the WorkspaceTemplate instance
+	workspaceTemplate := &infrastructurev1beta1.WorkspaceTemplate{}
+	if err := r.Get(ctx, req.NamespacedName, workspaceTemplate); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Unable to fetch WorkspaceTemplate")
+		return ctrl.Result{}, err
+	}
+
+	// Add finalizer if it doesn't exist
+	if !controllerutil.ContainsFinalizer(workspaceTemplate, workspaceTemplateFinalizerV2) {
+		controllerutil.AddFinalizer(workspaceTemplate, workspaceTemplateFinalizerV2)
+		if err := r.Update(ctx, workspaceTemplate); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Handle deletion
+	if !workspaceTemplate.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, workspaceTemplate)
+	}
+
+	// Handle normal reconciliation
+	return r.reconcileNormal(ctx, workspaceTemplate)
+}
+
+func (r *WorkspaceTemplateReconcilerV2) reconcileNormal(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// Create or update the Terraform Workspace
+	workspace := &tfv1beta1.Workspace{}
+	workspaceName := types.NamespacedName{
+		Name:      fmt.Sprintf("%s-workspace", workspaceTemplate.Name),
+		Namespace: workspaceTemplate.Namespace,
+	}
+
+	if err := r.Get(ctx, workspaceName, workspace); err != nil {
+		if apierrors.IsNotFound(err) {
+			// Create new workspace
+			if err := r.createWorkspace(ctx, workspaceTemplate, workspaceName); err != nil {
+				log.Error(err, "Failed to create Terraform Workspace")
+				return ctrl.Result{}, err
+			}
+			log.Info("Created Terraform Workspace", "workspace", workspaceName)
+		} else {
+			log.Error(err, "Failed to get Terraform Workspace")
+			return ctrl.Result{}, err
+		}
+	} else {
+		// Update existing workspace
+		if err := r.updateWorkspace(ctx, workspaceTemplate, workspace); err != nil {
+			log.Error(err, "Failed to update Terraform Workspace")
+			return ctrl.Result{}, err
+		}
+		log.Info("Updated Terraform Workspace", "workspace", workspaceName)
+	}
+
+	// Update status
+	workspaceTemplate.Status.WorkspaceName = workspaceName.Name
+
+	return ctrl.Result{}, nil
+}
+
+func (r *WorkspaceTemplateReconcilerV2) reconcileDelete(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// Delete the associated Terraform Workspace
+	workspace := &tfv1beta1.Workspace{}
+	workspaceName := types.NamespacedName{
+		Name:      fmt.Sprintf("%s-workspace", workspaceTemplate.Name),
+		Namespace: workspaceTemplate.Namespace,
+	}
+
+	if err := r.Get(ctx, workspaceName, workspace); err != nil {
+		if apierrors.IsNotFound(err) {
+			// Workspace is already gone, remove finalizer
+			controllerutil.RemoveFinalizer(workspaceTemplate, workspaceTemplateFinalizerV2)
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Delete the workspace
+	if err := r.Delete(ctx, workspace); err != nil {
+		log.Error(err, "Failed to delete Terraform Workspace")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("Deleted Terraform Workspace", "workspace", workspaceName)
+	return ctrl.Result{}, nil
+}
+
+func (r *WorkspaceTemplateReconcilerV2) createWorkspace(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate, name types.NamespacedName) error {
+	workspace := &tfv1beta1.Workspace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name.Name,
+			Namespace: name.Namespace,
+		},
+		Spec: tfv1beta1.WorkspaceSpec{
+			ForProvider: workspaceTemplate.Spec.Template.Spec,
+		},
+	}
+
+	// Set WriteConnectionSecretToRef if specified
+	if workspaceTemplate.Spec.WriteConnectionSecretToRef != nil {
+		workspace.Spec.WriteConnectionSecretToReference = workspaceTemplate.Spec.WriteConnectionSecretToRef
+	}
+
+	// Set owner reference
+	if err := ctrl.SetControllerReference(workspaceTemplate, workspace, r.Scheme); err != nil {
+		return fmt.Errorf("failed to set owner reference: %w", err)
+	}
+
+	return r.Create(ctx, workspace)
+}
+
+func (r *WorkspaceTemplateReconcilerV2) updateWorkspace(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate, workspace *tfv1beta1.Workspace) error {
+	// Update ForProvider with template spec
+	workspace.Spec.ForProvider = workspaceTemplate.Spec.Template.Spec
+
+	// Update WriteConnectionSecretToRef if specified
+	if workspaceTemplate.Spec.WriteConnectionSecretToRef != nil {
+		workspace.Spec.WriteConnectionSecretToReference = workspaceTemplate.Spec.WriteConnectionSecretToRef
+	}
+
+	return r.Update(ctx, workspace)
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *WorkspaceTemplateReconcilerV2) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&infrastructurev1beta1.WorkspaceTemplate{}).
+		Owns(&tfv1beta1.Workspace{}).
+		Complete(r)
+}

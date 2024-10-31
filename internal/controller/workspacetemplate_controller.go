81624b268d898df2b903ed68882ab36794c3ba24
reoring <reoring@gmail.com>
2024-10-31 17:39:44 +0900

    chore: add apply for workspacetemplate

--- internal/controller/workspacetemplate_controller.go
+++ internal/controller/workspacetemplate_controller.go
@@ -18,19 +18,15 @@ package controller
 
 import (
 	"context"
-	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	infrastructurev1beta1 "github.com/appthrust/capt/api/v1beta1"
-	tfv1beta1 "github.com/upbound/provider-terraform/apis/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -46,9 +42,6 @@ type WorkspaceTemplateReconciler struct {
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=workspacetemplates,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=workspacetemplates/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=workspacetemplates/finalizers,verbs=update
-//+kubebuilder:rbac:groups=tf.crossplane.io,resources=workspaces,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
-//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -82,110 +75,25 @@ func (r *WorkspaceTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Re
 	return r.reconcileNormal(ctx, workspaceTemplate)
 }
 
-func (r *WorkspaceTemplateReconciler) reconcileNormal(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate) (ctrl.Result, error) {
-	log := log.FromContext(ctx)
-
-	// Create or update the Terraform Workspace
-	workspace := &tfv1beta1.Workspace{}
-	workspaceName := types.NamespacedName{
-		Name:      fmt.Sprintf("%s-workspace", workspaceTemplate.Name),
-		Namespace: workspaceTemplate.Namespace,
-	}
-
-	if err := r.Get(ctx, workspaceName, workspace); err != nil {
-		if apierrors.IsNotFound(err) {
-			// Create new workspace
-			if err := r.createWorkspace(ctx, workspaceTemplate, workspaceName); err != nil {
-				log.Error(err, "Failed to create Terraform Workspace")
-				return ctrl.Result{}, err
-			}
-			log.Info("Created Terraform Workspace", "workspace", workspaceName)
-		} else {
-			log.Error(err, "Failed to get Terraform Workspace")
-			return ctrl.Result{}, err
-		}
-	} else {
-		// Update existing workspace
-		if err := r.updateWorkspace(ctx, workspaceTemplate, workspace); err != nil {
-			log.Error(err, "Failed to update Terraform Workspace")
-			return ctrl.Result{}, err
-		}
-		log.Info("Updated Terraform Workspace", "workspace", workspaceName)
-	}
-
-	// Update status
-	workspaceTemplate.Status.WorkspaceName = workspaceName.Name
-
+func (r *WorkspaceTemplateReconciler) reconcileNormal(_ context.Context, _ *infrastructurev1beta1.WorkspaceTemplate) (ctrl.Result, error) {
+	// Template validation logic can be added here
+	// For now, we just ensure the template exists and is valid
 	return ctrl.Result{}, nil
 }
 
 func (r *WorkspaceTemplateReconciler) reconcileDelete(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate) (ctrl.Result, error) {
-	log := log.FromContext(ctx)
-
-	// Delete the associated Terraform Workspace
-	workspace := &tfv1beta1.Workspace{}
-	workspaceName := types.NamespacedName{
-		Name:      fmt.Sprintf("%s-workspace", workspaceTemplate.Name),
-		Namespace: workspaceTemplate.Namespace,
-	}
-
-	if err := r.Get(ctx, workspaceName, workspace); err != nil {
-		if apierrors.IsNotFound(err) {
-			// Workspace is already gone, remove finalizer
-			controllerutil.RemoveFinalizer(workspaceTemplate, workspaceTemplateFinalizerV2)
-			return ctrl.Result{}, nil
-		}
+	// Remove finalizer
+	controllerutil.RemoveFinalizer(workspaceTemplate, workspaceTemplateFinalizerV2)
+	if err := r.Update(ctx, workspaceTemplate); err != nil {
 		return ctrl.Result{}, err
 	}
 
-	// Delete the workspace
-	if err := r.Delete(ctx, workspace); err != nil {
-		log.Error(err, "Failed to delete Terraform Workspace")
-		return ctrl.Result{}, err
-	}
-
-	log.Info("Deleted Terraform Workspace", "workspace", workspaceName)
 	return ctrl.Result{}, nil
-}
-
-func (r *WorkspaceTemplateReconciler) createWorkspace(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate, name types.NamespacedName) error {
-	workspace := &tfv1beta1.Workspace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-		},
-		Spec: workspaceTemplate.Spec.Template.Spec,
-	}
-
-	// Set WriteConnectionSecretToRef if specified
-	if workspaceTemplate.Spec.WriteConnectionSecretToRef != nil {
-		workspace.Spec.WriteConnectionSecretToReference = workspaceTemplate.Spec.WriteConnectionSecretToRef
-	}
-
-	// Set owner reference
-	if err := ctrl.SetControllerReference(workspaceTemplate, workspace, r.Scheme); err != nil {
-		return fmt.Errorf("failed to set owner reference: %w", err)
-	}
-
-	return r.Create(ctx, workspace)
-}
-
-func (r *WorkspaceTemplateReconciler) updateWorkspace(ctx context.Context, workspaceTemplate *infrastructurev1beta1.WorkspaceTemplate, workspace *tfv1beta1.Workspace) error {
-	// Update spec with template spec
-	workspace.Spec = workspaceTemplate.Spec.Template.Spec
-
-	// Update WriteConnectionSecretToRef if specified
-	if workspaceTemplate.Spec.WriteConnectionSecretToRef != nil {
-		workspace.Spec.WriteConnectionSecretToReference = workspaceTemplate.Spec.WriteConnectionSecretToRef
-	}
-
-	return r.Update(ctx, workspace)
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *WorkspaceTemplateReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&infrastructurev1beta1.WorkspaceTemplate{}).
-		Owns(&tfv1beta1.Workspace{}).
 		Complete(r)
 }
